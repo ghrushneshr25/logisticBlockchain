e064fef6e87f7cc237dec497ea79c388678bb87f
Ghrushnesh Rathod <ghrushneshr25@gmail.com>
2021-11-13 11:39:25 +0530

    difficulty and nonce added

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
+const INITIAL_DIFFICULTY = 3;
+
 const GENESIS_DATA = {
   timestamp: "12/11/2021",
   lastHash: "++++++",
   hash: "hash-init",
+  difficulty: INITIAL_DIFFICULTY,
+  nonce: 0,
   data: [],
 };
 
